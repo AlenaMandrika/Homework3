58da2b40ab484fdc9cba7a8bede1464601d29b10
alenamandrika <Alenenok0@ukr.net>
2017-10-23 11:54:19 +0300

    Homework3. MyForEach, MyMap, MySort

--- script.js
+++ script.js
@@ -20,3 +20,34 @@ var newArr = arr2.MyMap(function (item, i, arr) {
     return item*2
 });
 console.log(newArr);
+
+
+Array.prototype.MySort = function (arr) {
+    var length = this.length;
+    for (var i = 0; i < length - 1; i++) {
+        var min = i;
+        for (var j = i + 1; j < length; j++) {
+            if (this[j] < this[min]) {
+                min = j;
+            }
+            var t = this[min];
+            this[min] = this[i];
+            this[i] = t;
+        }
+    }
+    return arr
+};
+var arr3 = [1, 6, 9, 4, 9, -6, 5, -3];
+arr3.MySort(function (arr) {
+    if (a > b) {
+        return 1
+    }
+    if (a < b) {
+        return -1
+    }
+});
+console.log(arr3);
+
+var arr4 = ['h', 't', 'k', 's', 'a'];
+arr4.MySort();
+console.log(arr4);
