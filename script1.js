f9e8e90c715b6018f526010c0bf933b9aa8e33d6
alenamandrika <Alenenok0@ukr.net>
2017-10-25 18:36:56 +0300

    Homework3. Tamagotchi

--- script1.js
+++ script1.js
@@ -5,9 +5,10 @@ function Cat (name, age, view, colour ) {
     this.colour = colour || 'colour';
 
     var health = 0;
-    this.eat = function (fish) {
+    this.eat = function (eat) {
         health++;
-        console.log(this.name  + ' їсть ' + fish + ' ням-ням ')
+        console.log(this.name + ' ням-ням ');
+        toHelp('health')
     };
     this.becomeHealthier = function () {
         return health
@@ -16,7 +17,8 @@ function Cat (name, age, view, colour ) {
     var beauty = 0;
     this.drink = function (beverages) {
         beauty++;
-        console.log(this.name  +  " п'є "  +  beverages  +  ' мммм ' )
+        console.log(this.name  + ' мммм ' );
+        toHelp('beauty')
     };
     this.rejuvenate = function () {
         return beauty
@@ -25,7 +27,8 @@ function Cat (name, age, view, colour ) {
     var security = 0;
     this.sleep = function (hause) {
         security++;
-        console.log(this.name  +  ' спить ' + hause + ' шшшшшшш ')
+        console.log(this.name  + ' шшшшшшш ');
+        toHelp('security')
     };
     this.secure = function () {
         return security
@@ -34,7 +37,8 @@ function Cat (name, age, view, colour ) {
     var pleasure = 0;
     this.purr = function (mur) {
         pleasure++;
-        console.log(this.colour + this.view + this.name + ' муркає ' + mur + ' мрррр ')
+        console.log(this.colour + this.view + this.name + ' мрррр ');
+        toHelp('pleasure')
     };
     this.satisfied = function () {
         return pleasure
@@ -43,7 +47,8 @@ function Cat (name, age, view, colour ) {
     var leisure = 0;
     this.walk = function (walk) {
         leisure++;
-        console.log(this.name + ' гуляє ' + walk)
+        console.log(this.name + ' гуляє ');
+        toHelp('leisure')
     };
     this.play = function () {
         return leisure
@@ -52,45 +57,68 @@ function Cat (name, age, view, colour ) {
     var actions = 0;
     this.bite = function (bite) {
         actions++;
-        console.log(this.view  +  bite  + ' кусається  aaйй ');
+        console.log(this.view  + ' aaйй ');
         live()
     };
     this.misbehave = function () {
         return actions
     };
 
+    this.help = function () {
+       console.log('Angela is an animal who can perform such commands: ' +
+           ' "Angela.eat()" ' +
+           ' "Angela.drink()" ' +
+           ' "Angela.sleep()" ' +
+           ' "Angela.purr()" ' +
+           ' "Angela.walk()" ' +
+           ' "Angela.bite()" '
+       )
+    };
+    function toHelp (param) {
+        if (param === 'health') {
+            if (health <= 5) {
+                console.log('я голодна')
+            } else {
+                console.log('досить, пити')
+            }
+        }
+        if (param === 'beauty') {
+            if (beauty <= 5) {
+                console.log('хочу ще пити')
+            } else {
+                console.log('досить, спати час')
+            }
+        }
+        if (param === 'security') {
+            if (security <= 3) {
+                console.log('(((')
+            } else {
+                console.log(')))')
+            }
+        }
+        if (param === 'pleasure') {
+            if (pleasure <= 10) {
+                console.log('нема настрою')
+            } else {
+                console.log('хочу гуляти')
+            }
+        }
+        if (param === 'leisure') {
+            if (leisure <= 2) {
+                console.log('маловато')
+            } else {
+                console.log('a тепер би поїсти')
+            }
+        }
+    }
     function live () {
-       if (actions >= 10) {
+       if (actions >= 5) {
            console.log('Game OVER!')
        }
     }
 }
 var Angela = new Cat ('Angela', 'one Jear', 'animal', 'white');
+Angela.help();
 
-Angela.eat('');
-Angela.eat('');
-Angela.eat('');
-Angela.eat('');
-console.log('Angela.becomeHealthier',  Angela.becomeHealthier());
-
-Angela.drink('');
-console.log('Angela.rejuvenate',  Angela.rejuvenate());
-
-Angela.sleep('');
-console.log('Angela.secure',  Angela.secure());
-
-Angela.purr('');
-console.log('Angela.satisfied',  Angela.satisfied());
-
-Angela.walk('');
-console.log('Angela.play',  Angela.play());
-
-Angela.bite('');
-Angela.bite('');
-Angela.bite('');
-Angela.bite('');
-Angela.bite('');
-Angela.bite('');
-console.log('Angela.misbehave', Angela.misbehave());
 
 
